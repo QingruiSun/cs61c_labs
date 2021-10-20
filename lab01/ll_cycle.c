dd1d89472ad73932f17d0feaa02a11e99f07d1c0
QingruiSun <2902753024@qq.com>
2021-10-20 04:08:44 -0700

    finished check cycle

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -3,5 +3,21 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+  node *tortoise = head;
+  node *hare = head;
+  if (hare == NULL) {
     return 0;
+  }
+  while (hare != NULL) {
+    if ((hare == NULL) || (hare->next == NULL)) {
+      return 0;
+    }
+    hare = hare->next;
+    hare = hare->next;
+    tortoise = tortoise->next;
+    if (hare == tortoise) {
+      return 1;
+    }
+  }
+  return 0;
 }
