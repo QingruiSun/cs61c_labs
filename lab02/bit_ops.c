091136e7284f50a6da35fe93c95767120dffce5b
QingruiSun <2902753024@qq.com>
2021-10-24 08:29:45 -0700

    finished bit operation

--- lab02/bit_ops.c
+++ lab02/bit_ops.c
@@ -9,7 +9,7 @@ unsigned get_bit(unsigned x,
     // Returning -1 is a placeholder (it makes
     // no sense, because get_bit only returns
     // 0 or 1)
-    return -1;
+    return (x >> n) & 0x1;
 }
 // Set the nth bit of the value of x to v.
 // Assume 0 <= n <= 31, and v is 0 or 1
@@ -17,11 +17,20 @@ void set_bit(unsigned * x,
              unsigned n,
              unsigned v) {
     // YOUR CODE HERE
+    unsigned t = 0x1;
+    t = t << n;
+    t = ~t;
+    *x = *x & t;
+    v = v << n;
+    *x = *x | v;
 }
 // Flip the nth bit of the value of x.
 // Assume 0 <= n <= 31
 void flip_bit(unsigned * x,
               unsigned n) {
     // YOUR CODE HERE
+    unsigned t = 0x1;
+    t = t << n;
+    *x = *x ^ t;
 }
 
