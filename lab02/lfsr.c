e14ba6a95e675314e7234de208005e47be373329
QingruiSun <2902753024@qq.com>
2021-10-24 08:53:48 -0700

    finished linear feedback shift register

--- lab02/lfsr.c
+++ lab02/lfsr.c
@@ -6,5 +6,13 @@
 
 void lfsr_calculate(uint16_t *reg) {
     /* YOUR CODE HERE */
+  uint16_t reg0 = *reg & 0x1;
+  uint16_t reg1 = (*reg >> 2) & 0x1;
+  uint16_t reg2 = (*reg >> 3) & 0x1;
+  uint16_t reg3 = (*reg >> 5) & 0x1;
+  uint16_t t = ((reg0 ^ reg1) ^ reg2) ^ reg3;
+  t = t << 15;
+  *reg = *reg >> 1;
+  *reg = *reg | t;
 }
 
