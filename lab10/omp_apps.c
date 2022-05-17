67d82576aba39b866dd90f165b2e93d0c46e5241
liangmingliang <2902753024@qq.com>
2022-05-17 18:04:05 +0800

    finished dotp

--- lab10/omp_apps.c
+++ lab10/omp_apps.c
@@ -24,23 +24,23 @@ double dotp_manual_optimized(double* x, double* y, int arr_size) {
   double global_sum = 0.0;
 #pragma omp parallel
   {
+    double partial_sum = 0;
 #pragma omp for
     for (int i = 0; i < arr_size; i++)
+      partial_sum += x[i] * y[i];
 #pragma omp critical
-      global_sum += x[i] * y[i];
+    global_sum += partial_sum;
   }
+
   return global_sum;
 }
 
 // EDIT THIS FUNCTION PART 2
 double dotp_reduction_optimized(double* x, double* y, int arr_size) {
   double global_sum = 0.0;
-#pragma omp parallel
-  {
-#pragma omp for
-    for (int i = 0; i < arr_size; i++)
-#pragma omp critical
-      global_sum += x[i] * y[i];
+#pragma omp parallel for reduction(+:global_sum)
+  for (int i = 0; i < arr_size; i++) {
+    global_sum += x[i] * y[i];
   }
   return global_sum;
 }
